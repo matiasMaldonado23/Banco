fe60f82288764c621995bd3ad4aaa7a4565ae0f4
matiasMaldonado23 <>
2021-05-12 15:54:37 -0300

    Initial commit

---
+++ ConsoleApp1/Cliente.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    class Cliente
+    {
+        public string Nombre { get; set; }
+        public string apellido { get; set; }
+        public double Efectivo { get; set; }
+        public double saldo { get; set; }
+        public int cbu { get; set; } = 0;
+
+        public Cuenta Cuenta { get; set; }
+        public Cliente()
+        {
+            Cuenta = new Cuenta();
+            Efectivo = 0;
+            saldo = 0;
+        }
+        public void Acreditar(double monto)
+        {
+            saldo = saldo + monto * 0.8;
+            Cuenta.Acreditar(monto * 0.2);
+        }
+        public void Debitar()
+    }
+}
---
+++ ConsoleApp1/Cuenta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Cuenta
+    {
+        private int Contador { get; set; } = 0;
+        public int Cbu { get; set; }
+        public double Saldo { get; set; }
+
+        public Cuenta()
+        {
+            Cbu = ++Contador;
+            Saldo = 0;
+        }
+        public void Acreditar(double monto) => Saldo += monto;
+        public void Debitar(double monto) => Saldo -= monto;
+    }
+}
